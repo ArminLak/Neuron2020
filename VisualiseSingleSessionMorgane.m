e5beca917022ab8b0f8fa3ba97127ba6e5b07159
morganemoss <35966065+morganemoss@users.noreply.github.com>
2018-03-05 14:58:55 +0000

    Legend for stim aligned contrast

--- VisualiseSingleSessionMorgane.m
+++ VisualiseSingleSessionMorgane.m
@@ -1,9 +1,9 @@
 clear all
 %close all
 
-animal_name = 'ALK071'
-exp_date   = '2018-03-02'
-exp_series ='3'
+animal_name = 'ALK070'
+exp_date   = '2018-02-28'
+exp_series ='1'
 
 %--------------- useful information --------------------------------------
 % task event
@@ -14,7 +14,7 @@ exp_series ='3'
 % ------------------------------------------------------------------------
 % start and stop of time axis for plot (in second before and after the event)
 start = -1 % s
-stop=3     % s
+stop=2     % s
 
 % original data is sammple 12K per s. We downsample 10 times making it 1.2K
 % per s. We define 2 s before and 2 s after the event (4 * 1.2k) we stay
@@ -24,12 +24,24 @@ load('MiceExpInfoPhotoM')                                   % load beh data data
 sample_rate = 12000;                                        % photoM recording sampling rate
 downsampleScale = 10;                                       % factor downsampling the Ca responses
 
+
+%define time zones for normalisation after alignment
+
+preAlign = (0.1*(sample_rate/downsampleScale)*abs(start)):((sample_rate/downsampleScale)*abs(start)-70);
+postAlign = ((sample_rate/downsampleScale)*abs(start)+200):(0.9*(sample_rate/downsampleScale)*(abs(start)+stop));
+
+
 %---------------- colors for plotting-------------------------------------
 
-colorGray = [0 0 0
-    0.3 0.3 0.3
+colorGray4 = [0.8 0.8 0.8
     0.6 0.6 0.6
-    0.8 0.8 0.8];
+    0.3 0.3 0.3
+    0 0 0];
+colorGray3 = [0.8 0.8 0.8
+    0.4 0.4 0.4
+    0 0 0];
+colorGray2 = [0.7 0.7 0.7
+    0 0 0];
 colorGreen = [0 1 0
     0 0.8 0
     0 0.6 0
@@ -138,10 +150,10 @@ posRel = wheel.resetAtEvent(t, pos, eventTimes);
 
 % --------- make plots --------------
 %%
-figure; hold on
+f = figure; hold on
 subplot(7,2,1); % psych curve
 
-plot(unique(TrialTimingData(:,2)'), performance,'k','LineWidth',2,'Marker','o','MarkerFaceColor','k','MarkerSize',5)
+plot(unique(TrialTimingData(:,2)'), performance, 'color', [74/255 127/255 189/255],'LineWidth',2,'Marker','o','MarkerFaceColor', [74/255 127/255 189/255],'MarkerSize',3)
 xlabel('Contrast')
 xticks([min(Stimz) 0 max(Stimz)])
 title('Psych Curve')
@@ -162,7 +174,7 @@ hold on
 
 ax = gca;
 Visstart = 30; % visualise trace from this trial 
-Visstop  = 40;  % visualise trace up to this trial
+Visstop  = 46;  % visualise trace up to this trial
 for ievent = Visstart: Visstop
     
     h=rectangle(ax, 'Position',[TrialTimingData(ievent,13) -2 TrialTimingData(ievent,14)-TrialTimingData(ievent,13) 10],'EdgeColor',[1 1 1], 'FaceColor', [144/255 186/255 212/255 0.2]);
@@ -186,14 +198,14 @@ end
 a = downsample(TimeStamps,10);
 xlim([0 a(end)])
 xlabel ('Time (s)')
-ylabel ('Response')
+ylabel ('Response, {\Delta} F / F')
 title ('Ca responses')
 xlim([TrialTimingData(Visstart, 12) - 1  TrialTimingData(Visstop, 14) + 2])
 
 % find the max and min of signal: something like this:    
-%Max4Yaxis = max ( DeltaFoverF (TimeStamps (TrialTimingData(3, 12) - 1  TrialTimingData(ievent, 14) + 2)
+%Max4Yaxis = max ( DeltaFoverF (TimeStamps (((TrialTimingData(3, 12)-1):(TrialTimingData(ievent, 14) + 2)), 1)))
 
-ylim([-10 28])
+ylim([min(smooth(downsample(DeltaFoverF,10))) max(smooth(downsample(DeltaFoverF,10)))])
 
 % subplot(4,1,3);
 % plot(t(1:end-1)+FileAlignDelay-0.2, smooth(diff(posRel)./diff(t)),'k')
@@ -214,7 +226,7 @@ prepostmeandiff = nan(size(Raster_MatrixStim,1), 1); %pre-allocate
 
 for ievent = 1:size(Raster_MatrixStim,1)
     
-    prepostmeandiff(ievent) = mean(Raster_MatrixStim(ievent, 1600:3000) - mean(Raster_MatrixStim(ievent, 70:1100))); %difference between before and after; indicator of relative signal change 
+    prepostmeandiff(ievent) = mean(Raster_MatrixStim(ievent, postAlign) - mean(Raster_MatrixStim(ievent, preAlign))); %difference between before and after; indicator of relative signal change 
     
 end
 % next for each istim
@@ -232,7 +244,7 @@ end
 ylim([min(normalised)*1.2 max(normalised)*1.2])
 xticks([Stimz])
 xticklabels([Stimz])
-plot (Stimz, normalised')
+plot (Stimz, normalised', 'LineWidth',2,'Marker','o','MarkerFaceColor', [74/255 127/255 189/255],'MarkerSize',3, 'Color', [74/255 127/255 189/255])
 xlabel('Contrast')
 ylabel('Rel')
 
@@ -248,18 +260,30 @@ xticklabels([start:1:stop])
 
 c=1;
 for istim = StimzAbs
-    plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim,:)),'color',colorGray(c,:))
-    c=c+1;
+    h = plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim,:)),'color',colorGray4(c,:),'LineWidth',2)
     
+    if length(StimzAbs)==4
+        set(h, 'color', colorGray4(c,:))
+    elseif length(StimzAbs)==3
+        set(h, 'color', colorGray3(c,:))
+    elseif length(StimzAbs)==2
+        set(h, 'color', colorGray2(c,:))
+    end
+    
+    c=c+1;
+end
+    
+if length(StimzAbs)==4
+    l = legend(num2str(StimzAbs(1)), num2str(StimzAbs(2)), num2str(StimzAbs(3)),num2str(StimzAbs(4)),'location','best')
+elseif length(StimzAbs)==3
+    l = legend(num2str(StimzAbs(1)), num2str(StimzAbs(2)), num2str(StimzAbs(3)),'location','best')
+elseif length(StimzAbs)==2
+    l = legend(num2str(StimzAbs(1)), num2str(StimzAbs(2)), 'location', 'best')
 end
 
+title(l, 'Contrast')
 
-if length(Stimz)==4
-    legend(num2str(Stimz(1)), num2str(Stimz(2)), num2str(Stimz(3)),num2str(Stimz(4)),'location','best')
-elseif length(Stimz)==3
-    legend(num2str(Stimz(1)), num2str(Stimz(2)), num2str(Stimz(3)),'location','best')
-end
-
+ylabel('{\Delta} F / F')
 xlabel ('Time (s)')
 
 
@@ -269,8 +293,8 @@ c=1;
 
 for istim = StimzAbs
     
-    plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==1,:)),'color',colorGreen(c,:))
-    plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==0,:)),'color',colorRed(c,:))
+    plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==1,:)),'color',colorGreen(c,:),'LineWidth',2)
+    plot(nanmean(Raster_MatrixStim(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==0,:)),'color',colorRed(c,:),'LineWidth',2)
     
     c=c+1;
     
@@ -281,6 +305,8 @@ xlim([0 (stop-start)* sample_rate]/downsampleScale)
 xticks([0:(sample_rate/downsampleScale):((stop-start)* sample_rate/downsampleScale)])
 % 0:((stop*start)*sample_rate)/downsampleScale):(sample_rate/downsampleScale)
 xticklabels([start:1:stop])
+ylabel('{\Delta} F / F')
+xlabel('Time (s)')
 
 
 
@@ -295,16 +321,16 @@ c=1;
 
 for istim = StimzAbs
     
-    plot(nanmean(Raster_MatrixReward(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==1,:)),'color',colorGreen(c,:))
-    plot(nanmean(Raster_MatrixReward(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==0,:)),'color',colorRed(c,:))
+    plot(nanmean(Raster_MatrixReward(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==1,:)),'color',colorGreen(c,:),'LineWidth',2)
+    plot(nanmean(Raster_MatrixReward(abs(TrialTimingData(:,2))==istim & TrialTimingData(:,9)==0,:)),'color',colorRed(c,:),'LineWidth',2)
     
     c=c+1;
     
 end
 
 
-legend('reward','no reward','location','best')
-
+legend('Reward','No reward','location','best')
+ylabel('{\Delta} F / F')
 xlabel ('Time (s)')
 
 
@@ -323,8 +349,10 @@ xticklabels([start:1:stop])
 % ylim([-1 1])
 yticks([-1 0 1])
 title('Onset Beep Aligned')
+ylabel('{\Delta} F / F')
+xlabel('Time (s)')
 
-plot(nanmean(Raster_MatrixOnsetBeep), 'color', colorGray(1,:),'LineWidth',2)
+plot(nanmean(Raster_MatrixOnsetBeep), 'color', colorGray4(1,:),'LineWidth',2)
 
 
 %------------------------------------------ post stim response by contrast for errors and correct trials-----
@@ -347,7 +375,7 @@ plot(Stimz, poststim(:,1), 'color', [47/255 189/255 28/255], 'LineWidth', 2)
 hold on;
 plot(Stimz, poststim(:,2), 'color', [189/255 89/255 28/255], 'LineWidth', 2)
 
-ylabel('Rel')
+ylabel('{\Delta} F / F')
 xlabel('Contrast')
 xticks([Stimz])
 legend('Reward', 'No reward')
@@ -361,7 +389,7 @@ outcomediff = nan(size(Raster_MatrixReward,1), 1); %pre-allocate
 
 for ievent = 1:size(Raster_MatrixReward,1)
     
-    outcomediff(ievent) = mean(Raster_MatrixReward(ievent, 1600:3000) - mean(Raster_MatrixReward(ievent, 70:1100))); %difference between before and after; indicator of relative signal change 
+    outcomediff(ievent) = mean(Raster_MatrixReward(ievent, postAlign) - mean(Raster_MatrixReward(ievent, preAlign))); %difference between before and after; indicator of relative signal change 
     
 end
 
@@ -379,7 +407,7 @@ plot(Stimz, postoutcome(:,1), 'color', [47/255 189/255 28/255], 'LineWidth', 2)
 hold on;
 plot(Stimz, postoutcome(:,2), 'color', [189/255 89/255 28/255], 'LineWidth', 2)
 
-ylabel('Rel')
+ylabel('{\Delta} F / F')
 xlabel('Contrast')
 xticks([Stimz])
 legend('Reward', 'No reward')
