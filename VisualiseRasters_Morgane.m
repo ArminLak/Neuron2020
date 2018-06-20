b945e131ddbc8df5649ce3a4d8a34117635ce447
morganemoss <35966065+morganemoss@users.noreply.github.com>
2018-06-20 17:22:08 +0100

    added psych curve and all trials raster

--- VisualiseRasters_Morgane.m
+++ VisualiseRasters_Morgane.m
@@ -9,6 +9,17 @@ load('BehPhotoM_Exp23')
 RTLimit = 10; % in s, excluding trials with RT longer than this
 
 
+% ------------------ start stop times for task events ------------------
+
+sstart = -0.2; %stimulus 
+sstop = 0.8;
+
+astart = -0.5; %action
+astop = 0.5;
+
+rstart = -0.2; %reward
+rstop = 0.8;
+
 
 %%
 
@@ -26,9 +37,37 @@ for iSession = sessionz
     
     TempRewardData= BehPhotoM(animal_ID).Session(iSession).NeuronReward;
     
+    TrialTimingData = BehPhotoM(animal_ID).Session(iSession).TrialTimingData;
+    
+    TempStimz = unique(TrialTimingData(:,2));
+    
 
     % here you can use imagesc to make colourful plots
     % for each session lets make the following subplots
+    
+    f = figure('Position', [300 200 800 900]); hold on
+     
+    
+    %------------------------------- plot psychometric curve------------------
+   %% 
+    subplot(4, 7, 1);
+    
+    c = 1;
+    for istim = TempStimz'
+    
+        performance(c) = nanmean (TrialTimingData(TrialTimingData(:,2)==istim,3));
+%         RT(c) = nanmean (ReactionTime(TrialTimingData(:,2)==istim));
+    
+        c=c+1; 
+    end
+
+
+    plot(TempStimz, performance,'k')
+%%
+    
+    % ---------- raster for all trials in session in order of RT ---------
+
+
     
     % aling to stimulus, sorted based on RT
     
