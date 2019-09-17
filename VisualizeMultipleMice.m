8f18085f78b3c2985970bffbabdcff7698a22b93
morganemoss <35966065+morganemoss@users.noreply.github.com>
2019-09-17 17:29:02 +0100

    all figures now DONNEENEENENENE

--- VisualizeMultipleMice.m
+++ VisualizeMultipleMice.m
@@ -9,19 +9,19 @@ IpsiContra = 1; %in PSTHs visualise according to ipsi/contra stimulus (or action
 % list of animals
 
 % VTA,
-% Animals = [48 50 51 64]
-% load('BehPhotoM_Exp23_VTA')
+Animals = [48 50 51 64]
+load('BehPhotoM_Exp23_VTA')
 
 % NAC
 % Animals = [56 57 59 66]
 % load('BehPhotoM_Exp23_NAc')
 
 % DMS
-  Animals = [53, 62, 63, 71,72]  % 55 has 6 stimuli. so I will need to make some changes to be able to add this
-%          53, 55,62, 63,64, 68, 70, 71, 72 
-% 68 and 70 signals are not good, 64 the signal is ok but looks very
-% strange
-load('BehPhotoM_Exp23_DMS')
+%   Animals = [53, 62, 63, 71,72]  % 55 has 6 stimuli. so I will need to make some changes to be able to add this
+% %          53, 55,62, 63,64, 68, 70, 71, 72 
+% % 68 and 70 signals are not good, 64 the signal is ok but looks very
+% % strange
+% load('BehPhotoM_Exp23_DMS')
 
 
 smooth_factor = 100;
