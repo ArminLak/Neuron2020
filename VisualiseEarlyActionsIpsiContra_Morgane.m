3659b2fff60b960f8d0f7cb16c0fa4adc4790765
morganemoss <35966065+morganemoss@users.noreply.github.com>
2019-10-08 11:01:20 +0100

    now accepts uni hem animals

--- VisualiseEarlyActionsIpsiContra_Morgane.m
+++ VisualiseEarlyActionsIpsiContra_Morgane.m
@@ -1,10 +1,11 @@
 % Morgane October 2019
 
-% close all 
+close all 
 clear all
 
 %DMS
-Animals = [53, 62, 63, 71,72];
+% Animals = [53, 62, 63, 71,72];
+Animals = [72];
 load('BehPhotoM_Exp23_DMS')
 load('MiceExpInfoPhotoM.mat')
 
@@ -107,17 +108,45 @@ for iAnimal = Animals
     
 end
 %%
-earlyBehDataIpsi = [earlyBehDataLeft(earlyBehDataLeft(:,2)<0,:) ; earlyBehDataRight(earlyBehDataRight(:,2)>0, :)];
-earlyBehDataContra = [earlyBehDataLeft(earlyBehDataLeft(:,2)>0,:) ; earlyBehDataRight(earlyBehDataRight(:,2)<0, :)];
-lateBehDataIpsi = [lateBehDataLeft(lateBehDataLeft(:,2)<0,:) ; lateBehDataRight(lateBehDataRight(:,2)>0, :)];
-lateBehDataContra = [lateBehDataLeft(earlyBehDataLeft(:,2)>0,:) ; lateBehDataRight(lateBehDataRight(:,2)<0, :)];
+earlyBehDataIpsi = [];
+earlyBehDataContra = [];
+lateBehDataIpsi = [];
+lateBehDataContra = [];
 
-earlyActionRasterIpsi = [earlyActionRasterLeft(earlyBehDataLeft(:,2)<0,:) ; earlyActionRasterRight(earlyBehDataRight(:,2) >0,:)];
-earlyActionRasterContra = [earlyActionRasterLeft(earlyBehDataLeft(:,2)>0,:) ; earlyActionRasterRight(earlyBehDataRight(:,2) <0,:)];
-earlyActionRasterZero = [earlyActionRasterLeft(earlyBehDataLeft(:,2)==0,:) ; earlyActionRasterRight(earlyBehDataRight(:,2)==0,:)];
-lateActionRasterIpsi = [lateActionRasterLeft(lateBehDataLeft(:,2)<0,:) ; lateActionRasterRight(lateBehDataRight(:,2) >0,:)];
-lateActionRasterContra = [lateActionRasterLeft(lateBehDataLeft(:,2)>0,:) ; lateActionRasterRight(lateBehDataRight(:,2) <0,:)];
-lateActionRasterZero = [lateActionRasterLeft(lateBehDataLeft(:,2)==0,:) ; lateActionRasterRight(lateBehDataRight(:,2)==0,:)];
+earlyActionRasterIpsi = [];
+earlyActionRasterContra = [];
+earlyActionRasterZero = [];
+lateActionRasterIpsi = [];
+lateActionRasterContra = [];
+lateActionRasterZero = [];
+
+if ismember(ChanN, 1)
+    earlyBehDataIpsi = [earlyBehDataIpsi; earlyBehDataLeft(earlyBehDataLeft(:,2)<0,:)];
+    earlyBehDataContra = [earlyBehDataContra; earlyBehDataLeft(earlyBehDataLeft(:,2)>0,:)];
+    lateBehDataIpsi = [lateBehDataIpsi; lateBehDataLeft(lateBehDataLeft(:,2)<0,:)];
+    lateBehDataContra = [lateBehDataContra; lateBehDataLeft(earlyBehDataLeft(:,2)>0,:)];
+    
+    earlyActionRasterIpsi = [earlyActionRasterIpsi; earlyActionRasterLeft(earlyBehDataLeft(:,2)<0,:)];
+    earlyActionRasterContra = [earlyActionRasterContra; earlyActionRasterLeft(earlyBehDataLeft(:,2)>0,:)];
+    earlyActionRasterZero = [earlyActionRasterZero; earlyActionRasterLeft(earlyBehDataLeft(:,2)==0,:)];
+    lateActionRasterIpsi = [lateActionRasterIpsi; lateActionRasterLeft(lateBehDataLeft(:,2)<0,:)];
+    lateActionRasterContra = [lateActionRasterContra; lateActionRasterLeft(lateBehDataLeft(:,2)>0,:)];
+    lateActionRasterZero = [lateActionRasterZero; lateActionRasterLeft(lateBehDataLeft(:,2)==0,:)];
+end
+
+if ismember(ChanN, 2)
+    earlyBehDataIpsi = [earlyBehDataIpsi; earlyBehDataRight(earlyBehDataRight(:,2)>0, :)];
+    earlyBehDataContra = [earlyBehDataContra; earlyBehDataRight(earlyBehDataRight(:,2)<0, :)];
+    lateBehDataIpsi = [lateBehDataIpsi; lateBehDataRight(lateBehDataRight(:,2)>0, :)];
+    lateBehDataContra = [lateBehDataContra; lateBehDataRight(lateBehDataRight(:,2)<0, :)];
+    
+    earlyActionRasterIpsi = [earlyActionRasterIpsi; earlyActionRasterRight(earlyBehDataRight(:,2) >0,:)];
+    earlyActionRasterContra = [earlyActionRasterContra; earlyActionRasterRight(earlyBehDataRight(:,2) <0,:)];
+    earlyActionRasterZero = [earlyActionRasterZero; earlyActionRasterRight(earlyBehDataRight(:,2)==0,:)];
+    lateActionRasterIpsi = [lateActionRasterIpsi; lateActionRasterRight(lateBehDataRight(:,2) >0,:)];
+    lateActionRasterContra = [lateActionRasterContra; lateActionRasterRight(lateBehDataRight(:,2) <0,:)];
+    lateActionRasterZero = [lateActionRasterZero; lateActionRasterRight(lateBehDataRight(:,2)==0,:)];
+end
 
 
 figure; 
@@ -142,6 +171,7 @@ set(gca, 'ylim', [-0.4 1], 'xlim', [StartTime + (TimingVisualise(2,1)*sampleRate
         'YTick', [0 0.5 1], 'XTick', [StartTime + (TimingVisualise(2,1)*sampleRate), StartTime,  StartTime + (TimingVisualise(2,2)*sampleRate)], ...
         'XTickLabel', {'-0.7','0','0.7'},'TickDir','out','Box','off')
 
+
     
     
     
@@ -157,7 +187,7 @@ plot( smooth( mean(lateActionRasterZero), smooth_factor), 'color', IpsiContraCol
 c = c+1;
 
 for iStim = [0.12 0.25 0.5];
-plot( smooth ( mean ( lateActionRasterContra(abs(lateBehDataContra(:,2))==iStim,:)), smooth_factor), 'color', IpsiContraColor(5,:), 'LineWidth', 2)
+plot( smooth ( mean ( lateActionRasterContra(abs(lateBehDataContra(:,2))==iStim,:)), smooth_factor), 'color', IpsiContraColor(5,:), 'LineWidth', 2);
 c = c+1;
 end
 
@@ -192,6 +222,23 @@ set(gca, 'ylim', [-0.4 1], 'xlim', [StartTime + (TimingVisualise(2,1)*sampleRate
         'XTickLabel', {'-0.7','0','0.7'},'TickDir','out','Box','off')
 
 
+    figure; 
+    
+    stimz = [0.12 0.25 0.5]
+    for i = 1:length(stimz)
+byContraStimActionResponses(i,:) = mean(earlyActionRasterContra(abs(earlyBehDataContra(:,2))==stimz(i),:), 1);
+byIpsiStimActionResponses(i,:) = mean(earlyActionRasterIpsi(abs(earlyBehDataIpsi(:,2))==stimz(i),:), 1);
+    end
+
+plot( smooth(mean(byContraStimActionResponses),smooth_factor), 'color', IpsiContraColor2(2,:), 'LineWidth', 2)
+hold on;
+plot( smooth(mean(byIpsiStimActionResponses),smooth_factor), 'color', IpsiContraColor2(1,:), 'LineWidth', 2)
+    
+    
+set(gca, 'ylim', [0 0.5], 'xlim', [StartTime + (TimingVisualise(2,1)*sampleRate) StartTime + (TimingVisualise(2,2)*sampleRate)], ...
+        'YTick', [0 0.5 1], 'XTick', [StartTime + (TimingVisualise(2,1)*sampleRate), StartTime,  StartTime + (TimingVisualise(2,2)*sampleRate)], ...
+        'XTickLabel', {'-0.7','0','0.7'},'TickDir','out','Box','off')
+    
 
 %% functions
  
